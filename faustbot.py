d1ae8ce5fdc32d1e6b385e6d429e2ed206a2774a
Felix Schneider <fsfuenf@gmail.com>
2013-04-06 17:21:36 +0200

    Ignoring all phrases in parentheses, properly parsing command.

--- faustbot.py
+++ faustbot.py
@@ -7,44 +7,41 @@ class FaustBot(BotPlugin):
     def __init__(self):
         super(FaustBot, self).__init__()
         self.parseFaust()
-
+		
     def parseFaust(self):
-        faust = open("faust.txt", encoding="utf-8")
         sentences = {}
         characters = {}
-        line = faust.readline()
         i = 0
         newlines = 2
         lastSentence = 0
+		with open("faust.txt", encoding="utf-8") as faust:
+			for line in faust:
+				line = self.removeparentheses(line)
+				if line == "\n":
+					newlines += 1
+				#if the first few characters are uppercase,
+				#there is a new character speaking
+				elif (len(line) < 3 and line.isupper()) or line[:3].isupper():
+					characters[i+1] = line[:-2].title()
+					newlines = 0
+					lastSentence = 0
+				elif newlines < 2 and not line == "":
+					i += 1
+					newlines = 0
+					if lastSentence == 0:
+						sentences[i] = line[:-1] # no ugly \n
+					else:
+						sentences[lastSentence]+= " \\ " + line[:-1]
 
-        while line != "":
-            if line == "\n":
-                newlines += 1
-            #if the first few characters are uppercase,
-            #there is a new character speaking
-            elif (len(line) < 3 and line.isupper()) or line[:3].isupper():
-                characters[i+1] = line[:-2]
-                newlines = 0
-                lastSentence = 0
-            elif newlines < 2 and not line.startswith("("):
-                i += 1
-                newlines = 0
-                if lastSentence == 0:
-                    sentences[i] = line[:-1] # no ugly \n
-                else:
-                    sentences[lastSentence]+= " \\ " + line[:-1]
-
-                if line[-2] in ["!", ".", "?"]:
-                    lastSentence = 0
-                elif lastSentence == 0:
-                    lastSentence = i
-            line = faust.readline()
-
-        faust.close()
+					if line[-2] in ["!", ".", "?"]:
+						lastSentence = 0
+					elif lastSentence == 0:
+						lastSentence = i
+			
         self.sentences = sentences
         self.characters = characters
         self.lines = i
-
+	
     @botcmd
     def faust(self, mess, args):
         """ Print a random line from Goethe's "Faust" in sentence context """
@@ -52,10 +49,10 @@ class FaustBot(BotPlugin):
         line = 0
         err = ""
         if len(argss) != 0:
-            if argss[0].isnumeric():
-                i = int(argss[0])
-                if abs(i) > self.lines:
-                    err += "Faust hat nur " + str(self.lines) + " Verse.\n"
+			try:
+				i = int(argss[0])
+				if abs(i) > self.lines:
+                    err += "Faust hat nur " + str(self.lines) + " Verse. Du kriegst einen anderen:\n"
                 elif i == 0:
                     line = 1
                 else:
@@ -63,18 +60,33 @@ class FaustBot(BotPlugin):
                         line = self.lines - i + 1
                     else:
                         line = i
-
+			except ValueError:
+				err += "Das ist keine Zahl. Ich bin mir sicher. Nimm einen zufälligen Vers:\n"
+		
         if line == 0:
             line = random.randint(0, self.lines)
 
-        whosaidit = self.getNextSmaller(self.characters, line)
-        sentence = self.getNextSmaller(self.sentences, line)
+        whosaidit = self.getnextsmaller(self.characters, line)
+        sentence = self.getnextsmaller(self.sentences, line)
         return err + '"{0}", gesprochen von {1} in Vers {2}'.format(sentence, whosaidit, line)
-
-    def getNextSmaller(self, d, i):
+	
+    def getnextsmaller(self, d, i):
         last = -1
         for j in iter(sorted(d.keys())):
             if j > i:
                 break
             last = j
         return d[last]
+	
+	def removeparantheses(self, s):
+		if "(" not in s and ")" not in s:
+			return s
+		start = s.find("(")
+		if start > 0 and s[start-1] == " ":
+			start -= 1
+		if start < 0:
+			start = 0
+		end = s.find(")") + 1
+		if end < 0:
+			end = len(s)
+		return s[:start] + s[end:]
